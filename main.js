b353a77d979abeebd452c071ba935e5691e482a7
John Fairclough <johnmfairclough@gmail.com>
2020-08-20 09:41:55 +0100

    css

--- main.js
+++ main.js
@@ -91,12 +91,49 @@ function totalCost(product) {
     let cartCost = localStorage.getItem('totalCost');
 
     if(cartCost != null) {
+        cartCost = parseInt(cartCost);
         localStorage.setItem('totalCost', cartCost + product.price);
     } else {
-
     localStorage.setItem('totalCost', product.price);
     }
 }
 
 
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+
+    if( cartItems && productContainer ) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += 
+            `<div class="product">
+            <ion-icon name="close-circle"></ion-icon>
+            <img src="./images/${item.tag}.jpg">
+            <span class="item-name">${item.name}</span>
+            <div class="price">£${item.price}</div>
+            <div class="quantity">
+            <ion-icon name="remove-circle-outline"></ion-icon>
+            <span>${item.inCart}</span>
+            <ion-icon name="add-circle-outline"></ion-icon>
+            </div>
+            <div class="total">£${item.inCart * item.price}</div>
+            `
+        });
+
+        productContainer.innerHTML += ` 
+        <div class="basketTotalContainer>
+        <h4 class="basketTotalTitle>
+        Basket Total
+        </h4>
+        <h4 class="basketTotal">
+        £${cartCost}
+        </h4>
+        `;
+    }
+}
+
 onLoadCartNumbers();
+displayCart()
